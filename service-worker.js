9778d8932fb8ff62445a89e80c507214dc529e20
pmeec <pmeec@orange.fr>
2026-06-29 20:45:12 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,1 @@
-const CACHE='lanzarote-v2-luxe-real';
-const ASSETS=['./','index.html','manifest.json','service-worker.js','sea-lanzarote.svg','icon-192.png','icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='lanzarote-v3';const ASSETS=['./','./index.html','./manifest.json','./sea-lanzarote.svg','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
